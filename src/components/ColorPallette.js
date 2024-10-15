ea9b7d73a937bff4bf1c84669e17e8cec596842d
Houssein Msallem <houssein.msallem@gmail.com>
2024-10-15 18:43:39 +0100

    sstyled header

--- src/components/ColorPallette.js
+++ src/components/ColorPallette.js
@@ -1,2 +1,15 @@
-const colors = { primary: '#9D44B5', secondary: '#671F7B' };
+const colors = {
+  bg: '#F9F9Fb',
+  r: '#FF5480',
+  p: '#9283E0',
+  o: '#FFA599',
+  y: '#FFD581',
+  v: '#FE90EC',
+  b: '#27AEFF',
+  g: '#00DC90',
+  textDefault: '#000000',
+  textPassive: '#9B9B9B',
+  textQuote: '#9283E0',
+  textHover: '#FF5480',
+};
 export default colors;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { useNavigate } from 'react-router-dom';
 import { Link as ReactRouterLink } from 'react-router-dom';
 import { Link as ChakraLink } from '@chakra-ui/react';
-import { Image, Box } from '@chakra-ui/react';
+import { Image, Box, Button } from '@chakra-ui/react';
+import logo from '../assets/png/logo-no-background.png';
 import colors from './ColorPallette';
 import '../cssModules/Header.css';
 export default function Header() {
   const navigate = useNavigate();
-  const primary = colors.primary;
-  const secondary = colors.secondary;
+  const bg = colors.bg;
+  const textDefault = colors.textDefault;
+  const textHover = colors.textHover;
+  const ctaColor = colors.p;
   const userId = localStorage.getItem('userId');
   const handleLogout = () => {
     localStorage.removeItem('jwtToken');
@@ -16,125 +19,94 @@ export default function Header() {
   };
   if (!userId) {
     return (
-      <Box className="headerContainer">
-        <Box className="headerLeft">
-          <Image w={10} h={8} className="logo" />
-          <ChakraLink
-            className="link"
-            as={ReactRouterLink}
-            to="/"
-            bg="#22222"
-            color={primary}
-            padding={'6px'}
-            border={('solid', '2px')}
-            textDecoration={'none'}
-            borderRadius={'5px'}
-          >
-            Home
-          </ChakraLink>
-        </Box>
+      <Box className="headerContainer" bg={bg}>
+        <Image h={8} className="logo" src={logo} />
+
         <Box
           display={'flex'}
           justifyContent={'center'}
           alignItems={'center'}
-          gap={'10px'}
+          gap={'6em'}
           className="headerRight"
         >
           <ChakraLink
-            size="sm"
-            bg={secondary}
-            color="white"
-            _hover={{ bg: '#9D44B5' }}
+            className="link"
             as={ReactRouterLink}
-            to={'/login'}
-            className="login "
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            textDecoration={'none'}
-            padding={'6px'}
-            borderRadius={'5px'}
+            to="/"
+            color={textDefault}
+            _hover={{ color: textHover }}
           >
-            {' '}
-            Login
+            Home
           </ChakraLink>
 
           <ChakraLink
             size="sm"
-            bg="#22222"
             textDecoration={'none'}
-            color={primary}
+            color={textDefault}
             as={ReactRouterLink}
+            _hover={{ color: textHover }}
             to={'/signup'}
-            className="link"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            padding={('4px', '6px', '4px', '4px')}
-            border={('solid', '2px')}
-            borderRadius={'5px'}
           >
-            {' '}
             Register
           </ChakraLink>
+          <ChakraLink
+            size="sm"
+            color={textDefault}
+            as={ReactRouterLink}
+            to={'/login'}
+            _hover={{ color: textHover }}
+          >
+            Login
+          </ChakraLink>
+          <Button color={bg} bg={ctaColor}>
+            Subscribe
+          </Button>
         </Box>
       </Box>
     );
   }
   return (
-    <Box className="headerContainer">
-      <Box className="headerLeft">
-        <Image w={10} h={8} className="logo" />
-        <ChakraLink
-          as={ReactRouterLink}
-          to="/"
-          bg="#22222"
-          color={primary}
-          className="link"
-          textDecoration={'none'}
-          padding={'6px'}
-          border={('solid', '2px')}
-          borderRadius={'5px'}
-        >
-          Home
-        </ChakraLink>
-      </Box>
+    <Box className="headerContainer" bg={bg}>
+      <Image w={10} h={8} className="logo" src={logo} />
       <Box
         display={'flex'}
         justifyContent={'center'}
         alignItems={'center'}
-        gap={'10px'}
+        gap={'6em'}
         className="headerRight"
       >
         <ChakraLink
-          size="sm"
-          bg={secondary}
-          color="white"
-          fontWeight={'bold'}
-          _hover={{ bg: '#9D44B5' }}
-          textDecoration={'none'}
+          className="link"
+          as={ReactRouterLink}
+          to="/"
+          color={textDefault}
+          _hover={{ color: textHover }}
+        >
+          Home
+        </ChakraLink>
+        <ChakraLink
+          className="link"
           as={ReactRouterLink}
           to="/posts"
-          className="link"
-          padding={'6px'}
-          borderRadius={'5px'}
+          color={textDefault}
+          _hover={{ color: textHover }}
         >
           Browse
         </ChakraLink>
 
         <ChakraLink
-          size="sm"
-          bg="#22222"
-          color={primary}
-          onClick={handleLogout}
-          textDecoration={'none'}
           className="link"
-          padding={'4px'}
-          borderRadius={'5px'}
-          border={('solid', '2px')}
+          as={ReactRouterLink}
+          to="/"
+          color={textDefault}
+          _hover={{ color: textHover }}
+          onClick={handleLogout}
         >
           Logout
         </ChakraLink>
+        <Button color={bg} bg={ctaColor}>
+          Subscribe
+        </Button>
       </Box>
     </Box>
   );
